25b9ec711a12f886c2285ad01ec90ee87e88822e
tampohseng0 <tanpohseng0@gmail.com>
2013-07-10 05:24:07 +0000

    read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+//  response.send('Hello World2!');
+
+    fs = require("fs");
+//    mysting = fs.readFileSync('index.html', 'utf8');
+
+    var mybuffer = fs.readFileSync('index.html');
+    var mystring = mybuffer.toString('utf8');
+    response.send(mytsring);
+
 });
 
 var port = process.env.PORT || 5000;
