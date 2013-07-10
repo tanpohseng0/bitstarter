ade0425baf6ac75fe72a11ef712e76e5156796eb
tampohseng0 <tanpohseng0@gmail.com>
2013-07-10 05:38:45 +0000

    web.js

--- web.js
+++ web.js
@@ -5,10 +5,10 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 //  response.send('Hello World2!');
 
-    fs = require("fs");
+  
 //    mysting = fs.readFileSync('index.html', 'utf8');
 
-    var mybuffer = fs.readFileSync('index.html');
+    var mybuffer = fs.readFileSync("index.html");
     var mystring = mybuffer.toString('utf8');
     response.send(mytsring);
 
